5e5aaab445f47b2161bc142a15bb5c2e7f4a3b91
marcos-utn <elcorreodemarcos@gmail.com>
2015-10-29 05:46:27 -0300

    initial setup

---
+++ app/src/main/java/com/example/android/viewpagerproject_clase10/Pantalla3Fragment.java
@@ -0,0 +1,25 @@
+package com.example.android.viewpagerproject_clase10;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Pantalla3Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle
+            savedInstanceState) {
+
+        // Creamos la View para este fragment y la devolvemos
+        ViewGroup rootView = (ViewGroup)
+                inflater.inflate(R.layout.layout_contenido3,
+                        container, false);
+
+        // Cargamos la View con contenido, seteamos listeners, etc.
+        //...
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/example/android/viewpagerproject_clase10/ScreenSlidePagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.android.viewpagerproject_clase10;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by android on 29/10/15.
+ */
+public class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter{
+    private static final int NUM_PAGES = 3;
+
+    public ScreenSlidePagerAdapter(FragmentManager fm)
+    {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position)
+    {
+        // Segun la posicion, creo el fragment correspondiente y lo devuelvo
+        switch(position)
+        {
+            case 0 : return new Pantalla1Fragment();
+            case 1 : return new Pantalla2Fragment();
+            case 2 : return new Pantalla3Fragment();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount()
+    {
+        return NUM_PAGES;
+    }
+}
